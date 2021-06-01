bc93f8b3f46bb87b871166145aa57f5e650892d4
mustaphash <79004067+mustaphash@users.noreply.github.com>
2021-06-01 23:12:48 +0300

    Add files via upload

---
+++ srs/Models/Place.cs
@@ -0,0 +1,20 @@
+﻿namespace Models
+{
+    public class Place
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public double Longitude { get; set; }
+
+        public double Latitude { get; set; }
+
+        public string Address { get; set; }
+
+        public string Category { get; set; }
+
+    }
+}
