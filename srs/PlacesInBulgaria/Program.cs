ada0a5a19853ca7555527bc992ea7ddbaeaff67b
mustaphash <79004067+mustaphash@users.noreply.github.com>
2021-06-01 23:18:31 +0300

    Add files via upload

---
+++ srs/PlacesInBulgaria/Program.cs
@@ -0,0 +1,27 @@
+﻿using Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PlacesInBulgaria
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync($"http://62.171.141.18/?fbclid=IwAR3YE74zv_w-pN6hJNNq5_oljKj3821fsXgupggehcRF2gZ9SmQX72L3sME");
+            string content = await response.Content.ReadAsStringAsync();
+            
+            var places = JsonConvert.DeserializeObject<List<Place>>(content);
+
+            foreach (var place in places)
+            {
+                Console.WriteLine(place.Address);
+            }
+        }
+    }
+}
