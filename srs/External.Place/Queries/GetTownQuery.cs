f36da0296edb75676dabbe2a8722db032e3ed3a3
Mustafa Shakir <mshakir9918@gmail.com>
2021-06-18 19:23:31 +0300

    the true road for finish :)

--- srs/External.Place/Queries/GetTownQuery.cs
+++ srs/External.Place/Queries/GetTownQuery.cs
@@ -1,6 +1,9 @@
 ﻿using External.Places.Queries.Interfaces;
 using Models.Place;
 using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -20,15 +23,20 @@ namespace External.Places.Queries
             _httpClientContext = httpClientContext;
         }
 
-        public async Task<Place> ExecuteAsync() 
+        public async Task<Place> ExecuteAsync()
         {
             var httpClient = _httpClientContext.GetClient();
             HttpResponseMessage response = await httpClient.GetAsync($"http://62.171.141.18/?fbclid=IwAR3YE74zv_w-pN6hJNNq5_oljKj3821fsXgupggehcRF2gZ9SmQX72L3sME");
             string content = await response.Content.ReadAsStringAsync();
-
-            var places = JsonConvert.DeserializeObject<Place>(content);
+            string answer = Console.ReadLine();
+            var places = JsonConvert.DeserializeObject<List<Place>>(content);
+            places = (List<Place>)places.Where(p => p.Address.Contains(answer));
             
-            return places;
+                foreach (Place place in places)
+                {
+                    Console.WriteLine(place.Address);
+                }
+            return ;
         }
 
     }
--- srs/PlacesInBulgaria/Program.cs
+++ srs/PlacesInBulgaria/Program.cs
@@ -1,5 +1,6 @@
 ﻿using External.Places.Queries;
 using External.Places.Queries.Interfaces;
+using Models.Place;
 using System;
 using System.Device.Location;
 using System.Linq;
@@ -18,9 +19,13 @@ namespace PlacesInBulgaria
             Console.WriteLine("3-Географска дължина и широчина");
             Console.WriteLine("4-Име");
 
-            string answer;
+            //string answer;
             IGetTownQuery townName = new GetTownQuery();
             var town = await townName.ExecuteAsync();
+            foreach (var towns in GetTownQuery)
+            {
+
+            }
 
             IGetCategoryQuery townCategory = new GetCategoryQuery();
             var category = await townCategory.ExecuteAsync();
@@ -31,69 +36,70 @@ namespace PlacesInBulgaria
             IGetNameQuery townNames = new GetNameQuery();
             var names = await townNames.ExecuteAsync();
 
+            Console.WriteLine(town.Address);
 
-           while (true)
-           { 
-            Console.Write("Отговор: ");
-            int number = int.Parse(Console.ReadLine());
-           
-            if (number == 1)
-            {
-                Console.Write("Напишете област, моля: ");
-                answer = Console.ReadLine();
-                var filtredPlaces =await town.Where(p => p.Address.Contains(answer));
-           
-                foreach (var place in filtredPlaces)
-                {
-                    Console.Write($"{place.Name}");
-                    Console.WriteLine();
-                }
-            }
-            else if (number == 2)
-            {
-                Console.Write("Напишете категория, моля: ");
-                answer = Console.ReadLine();
-                var filtredCategory = places.Where(c => c.Category.Contains(answer));
-           
-                foreach (var category in filtredCategory)
-                {
-                    Console.WriteLine($"{category.Name}");
-                }
-            }
-            else if (number == 3)
-            {
-                // 42.72292889087154, 23.248363872647936
-                var latitude = 42.72292889087154; //double.Parse(Console.ReadLine());
-                var longitude = 23.248363872647936; //double.Parse(Console.ReadLine());
-                var coord = new GeoCoordinate(latitude, longitude);
-           
-                var nearest = places.Select(x =>
-                new
-                {
-                    Place = x,
-                    Location = new GeoCoordinate(x.Latitude, x.Longitude)
-                })
-                    .OrderBy(x => x.Location.GetDistanceTo(coord))
-                    .First();
-           
-                Console.WriteLine(nearest.Place.Name);
-                Console.WriteLine(nearest.Place.Description);
-                Console.WriteLine(nearest.Place.Address);
-           
-           
-            }
-            else if (number == 4)
-            {
-                Console.Write("Напишете името на местността: ");
-                answer = Console.ReadLine();
-                var filtredName = places.Where(n => n.Name.Contains(answer));
-                foreach (var name in filtredName)
-                {
-                    Console.WriteLine(name.Description);
-                }
-            }
-
-            }
+         // while (true)
+         // { 
+         //  Console.Write("Отговор: ");
+         //  int number = int.Parse(Console.ReadLine());
+         // 
+         //  if (number == 1)
+         //  {
+         //      Console.Write("Напишете област, моля: ");
+         //      answer = Console.ReadLine();
+         //      var filtredPlaces =await town.Where(p => p.Address.Contains(answer));
+         // 
+         //      foreach (var place in filtredPlaces)
+         //      {
+         //          Console.Write($"{place.Name}");
+         //          Console.WriteLine();
+         //      }
+         //  }
+         //  else if (number == 2)
+         //  {
+         //      Console.Write("Напишете категория, моля: ");
+         //      answer = Console.ReadLine();
+         //      var filtredCategory = places.Where(c => c.Category.Contains(answer));
+         // 
+         //      foreach (var category in filtredCategory)
+         //      {
+         //          Console.WriteLine($"{category.Name}");
+         //      }
+         //  }
+         //  else if (number == 3)
+         //  {
+         //      // 42.72292889087154, 23.248363872647936
+         //      var latitude = 42.72292889087154; //double.Parse(Console.ReadLine());
+         //      var longitude = 23.248363872647936; //double.Parse(Console.ReadLine());
+         //      var coord = new GeoCoordinate(latitude, longitude);
+         // 
+         //      var nearest = places.Select(x =>
+         //      new
+         //      {
+         //          Place = x,
+         //          Location = new GeoCoordinate(x.Latitude, x.Longitude)
+         //      })
+         //          .OrderBy(x => x.Location.GetDistanceTo(coord))
+         //          .First();
+         // 
+         //      Console.WriteLine(nearest.Place.Name);
+         //      Console.WriteLine(nearest.Place.Description);
+         //      Console.WriteLine(nearest.Place.Address);
+         // 
+         // 
+         //  }
+         //  else if (number == 4)
+         //  {
+         //      Console.Write("Напишете името на местността: ");
+         //      answer = Console.ReadLine();
+         //      var filtredName = places.Where(n => n.Name.Contains(answer));
+         //      foreach (var name in filtredName)
+         //      {
+         //          Console.WriteLine(name.Description);
+         //      }
+         //  }
+         //
+         //  }
         }
     }
 }
