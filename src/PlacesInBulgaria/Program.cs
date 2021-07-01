dc675acac178d4e705f3cb919bb605cdd02c3255
Mustafa Shakir <mshakir9918@gmail.com>
2021-07-01 18:38:13 +0300

    Create parser class for longitude and latitude and exeption

--- src/PlacesInBulgaria/Program.cs
+++ src/PlacesInBulgaria/Program.cs
@@ -1,69 +1,21 @@
 ﻿using CommandLine;
-using ConsoleTableExt;
-using External.Places.Queries;
-using External.Places.Queries.Interfaces;
 using PlacesInBulgaria.Verbs;
 using PlacesInBulgaria.Verbs.Parsers;
-using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Threading.Tasks;
 
 namespace PlacesInBulgaria
 {
     class Program
     {
-        
-
-        static async Task<int> ParseLongAndLat(LongAndLatVerb longitude, LongAndLatVerb latitude)
-        {
-            IGetLngAndLatQuery townLongAndLat = new GetLongAndLatQuery();
-            var longAndLat = await townLongAndLat.ExecuteAsync(longitude.Longitude, latitude.Latitude);
-
-            DataTable longLatTable = new DataTable($"Забележителности за географска дължина и широчина {longAndLat}");
-
-            longLatTable.Columns.Add("Номер");
-            longLatTable.Columns.Add("Име");
-            longLatTable.Columns.Add("Описание");
-            longLatTable.Columns.Add("Категория");
-
-            int i = 1;
-            foreach (var longiLat in longAndLat)
-            {
-                longLatTable.Rows.Add(
-                    i,
-                    longiLat.Name,
-                    longiLat.Description,
-                    longiLat.Category);
-
-                i++;
-            }
-
-            ConsoleTableBuilder
-                .From(longLatTable)
-                .WithFormat(ConsoleTableBuilderFormat.Alternative)
-                .ExportAndWriteLine();
-
-            return 0;
-        }
-
-        
-
-        static Task<int> ExceptionHandling(IEnumerable<Error> errors)
-        {
-            Console.WriteLine("Something went wrong.");
-            return Task.FromResult(0);
-        }
-
         static void Main(string[] args)
         {
-            Parser.Default.ParseArguments<AreaVerb, CategoryVerb, NameVerb,LongAndLatVerb>(args)
+            Parser.Default.ParseArguments<AreaVerb, CategoryVerb, NameVerb, LongAndLatVerb>(args)
                 .MapResult(
                   (AreaVerb opts) => new AreaParser().Parse(opts).GetAwaiter().GetResult(),
                   (CategoryVerb opts) => new CategoryParser().ParseCategory(opts).GetAwaiter().GetResult(),
                   (NameVerb opts) => new NameParser().ParseName(opts).GetAwaiter().GetResult(),
-                  (LongAndLatVerb opts) => ParseLongAndLat(opts, opts).GetAwaiter().GetResult(),
-                  (IEnumerable<Error> errs) => ExceptionHandling(errs).GetAwaiter().GetResult());
+                  (LongAndLatVerb opts) => new LongAndLatParser().ParseLongAndLat(opts, opts).GetAwaiter().GetResult(),
+                  (IEnumerable<Error> errs) => new ExeptionParser().ExceptionHandling(errs).GetAwaiter().GetResult());
         }
     }
 }
---
+++ src/PlacesInBulgaria/Verbs/Parsers/LongAndLatParser.cs
@@ -0,0 +1,43 @@
+﻿using ConsoleTableExt;
+using External.Places.Queries;
+using External.Places.Queries.Interfaces;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace PlacesInBulgaria.Verbs.Parsers
+{
+    class LongAndLatParser
+    {
+        public async Task<int> ParseLongAndLat(LongAndLatVerb longitude, LongAndLatVerb latitude)
+        {
+            IGetLngAndLatQuery townLongAndLat = new GetLongAndLatQuery();
+            var longAndLat = await townLongAndLat.ExecuteAsync(longitude.Longitude, latitude.Latitude);
+
+            DataTable longLatTable = new DataTable($"Забележителности за географска дължина и широчина {longAndLat}");
+
+            longLatTable.Columns.Add("Номер");
+            longLatTable.Columns.Add("Име");
+            longLatTable.Columns.Add("Описание");
+            longLatTable.Columns.Add("Категория");
+
+            int i = 1;
+            foreach (var longiLat in longAndLat)
+            {
+                longLatTable.Rows.Add(
+                    i,
+                    longiLat.Name,
+                    longiLat.Description,
+                    longiLat.Category);
+
+                i++;
+            }
+
+            ConsoleTableBuilder
+                .From(longLatTable)
+                .WithFormat(ConsoleTableBuilderFormat.Alternative)
+                .ExportAndWriteLine();
+
+            return 0;
+        }
+    }
+}
