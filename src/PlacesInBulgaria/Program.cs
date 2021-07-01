b2a6eaae8f5099274f142d6a3f5f8cac858e2a10
Mustafa Shakir <mshakir9918@gmail.com>
2021-07-01 18:30:15 +0300

    Create parser class for Category

--- src/PlacesInBulgaria/Program.cs
+++ src/PlacesInBulgaria/Program.cs
@@ -13,41 +13,7 @@ namespace PlacesInBulgaria
 {
     class Program
     {
-        static async Task<int> ParseCategory(CategoryVerb category)
-        {
-            Console.WriteLine("Choosed category");
-
-            IGetCategoryQuery townCategory = new GetCategoryQuery();
-            var categoryNames = await townCategory.ExecuteAsync("история и култура");
-
-            DataTable dataTable = new DataTable($"Забележителности за категория {categoryNames}");
-
-            dataTable.Columns.Add("Номер");
-            dataTable.Columns.Add("Име");
-            dataTable.Columns.Add("Адрес");
-            dataTable.Columns.Add("Географска дължина");
-            dataTable.Columns.Add("Географска широчина");
-
-            int i = 1;
-            foreach (var categoryName in categoryNames)
-            {
-                dataTable.Rows.Add(
-                    i,
-                    categoryName.Name,
-                    categoryName.Address,
-                    categoryName.Latitude,
-                    categoryName.Longitude);
-
-                i++;
-            }
-
-            ConsoleTableBuilder
-                .From(dataTable)
-                .WithFormat(ConsoleTableBuilderFormat.Alternative)
-                .ExportAndWriteLine();
-
-            return 0;
-        }
+        
 
         static async Task<int> ParseLongAndLat(LongAndLatVerb longitude, LongAndLatVerb latitude)
         {
@@ -121,7 +87,7 @@ namespace PlacesInBulgaria
             Parser.Default.ParseArguments<AreaVerb, CategoryVerb, NameVerb,LongAndLatVerb>(args)
                 .MapResult(
                   (AreaVerb opts) => new AreaParser().Parse(opts).GetAwaiter().GetResult(),
-                  (CategoryVerb opts) => ParseCategory(opts).GetAwaiter().GetResult(),
+                  (CategoryVerb opts) => new CategoryParser().ParseCategory(opts).GetAwaiter().GetResult(),
                   (NameVerb opts) => ParseName(opts).GetAwaiter().GetResult(),
                   (LongAndLatVerb opts) => ParseLongAndLat(opts, opts).GetAwaiter().GetResult(),
                   (IEnumerable<Error> errs) => ExceptionHandling(errs).GetAwaiter().GetResult());
---
+++ src/PlacesInBulgaria/Verbs/Parsers/CategoryParser.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleTableExt;
+using External.Places.Queries;
+using External.Places.Queries.Interfaces;
+using System;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace PlacesInBulgaria.Verbs.Parsers
+{
+    public class CategoryParser
+    {
+        public async Task<int> ParseCategory(CategoryVerb category)
+        {
+            Console.WriteLine("Choosed category");
+
+            IGetCategoryQuery townCategory = new GetCategoryQuery();
+            var categoryNames = await townCategory.ExecuteAsync("история и култура");
+
+            DataTable dataTable = new DataTable($"Забележителности за категория {categoryNames}");
+
+            dataTable.Columns.Add("Номер");
+            dataTable.Columns.Add("Име");
+            dataTable.Columns.Add("Адрес");
+            dataTable.Columns.Add("Географска дължина");
+            dataTable.Columns.Add("Географска широчина");
+
+            int i = 1;
+            foreach (var categoryName in categoryNames)
+            {
+                dataTable.Rows.Add(
+                    i,
+                    categoryName.Name,
+                    categoryName.Address,
+                    categoryName.Latitude,
+                    categoryName.Longitude);
+
+                i++;
+            }
+
+
+            ConsoleTableBuilder
+                .From(dataTable)
+                .WithFormat(ConsoleTableBuilderFormat.Alternative)
+                .ExportAndWriteLine();
+
+            return 0;
+        }
+    }
+}
